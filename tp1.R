964ab992a016baf8e675c8cf01dcc4a393eda37d
gcasado0 <casado.gustavo@gmail.com>
2025-06-09 19:51:27 -0300

    correccion error en grafico ingreso total e inferencia de ingreso total

--- tp1.R
+++ tp1.R
@@ -1,10 +1,14 @@
 install.packages("eph")
 install.packages("tidyverse")
+install.packages("nortest")
+
 library(eph)
 library(tidyverse)
 library(ggplot2)
 library(scales)
 library(dplyr)
+library(DescTools)
+library(nortest)
 
 # Descargar los microdatos del primer trimestre de 2024
 datos <- get_microdata(year = 2024, trimester = 1, type = "individual")
@@ -13,7 +17,8 @@ datos <- get_microdata(year = 2024, trimester = 1, type = "individual")
 #  Filtramos los datos para incluir solo personas en edad laboral (18-65 años)
 # -------------------------------------------------------------------
 datos_edad_entre_18y65 <- datos %>%
-  filter(CH06 >= 18 & CH06 <= 65)
+  filter(CH06 >= 18 & CH06 <= 65) %>%
+  select(CH06, CH15, P47T, NIVEL_ED, ESTADO)
 
 # -------------------------------------------------------------------
 # Creamos un subconjuntos para argentinos y extranjeros
@@ -123,7 +128,7 @@ datos_ingresos <- datos_combinados_entre_18y65 %>% filter(P47T >= 0)
 limite_superior <- quantile(datos_ingresos$P47T, 0.99, na.rm = TRUE)
 datos_filtrados <- datos_ingresos %>% filter(P47T >= 0 & P47T < limite_superior)
 
-ingresos_media_mediana <- datos_combinados_entre_18y65 %>%
+ingresos_media_mediana <- datos_filtrados %>%
   group_by(origen) %>%
   summarise(
     media = mean(P47T, na.rm = TRUE),
@@ -141,11 +146,72 @@ ggplot(datos_filtrados, aes(x = origen, y = P47T, fill = origen)) +
   ) +
   # Agregar texto con las estadísticas
   geom_text(data = ingresos_media_mediana, aes(x = origen, y = media, label = paste0("Media: ", round(media, 0))),
-            color = "darkred", vjust = -0.7, hjust = 0.2) + # Anotar media
+            color = "darkred", vjust = -0.7, hjust = 0.1) + # Anotar media
   geom_text(data = ingresos_media_mediana, aes(x = origen, y = mediana, label = paste0("Mediana: ", round(mediana, 0))),
-            color = "darkblue", vjust = 5, hjust = 0.2) + # Anotar mediana
+            color = "darkblue", vjust = 8, hjust = 0.1) + # Anotar mediana
   coord_flip() +
   theme_minimal()
+
+#Vamos a tomar los datos de ingresos sin filtrar por limite superior
+ingresos_argentinos <- (argentinos_entre_18y65 %>% filter(P47T >= 0))$P47T
+ingresos_extranjeros <-(extranjeros_entre_18y65 %>% filter(P47T >= 0))$P47T
+summary(ingresos_argentinos)
+summary(ingresos_extranjeros)
+
+#histograma
+hist(ingresos_argentinos,
+     main = "Histograma de P47T",
+     xlab = "Ingreso (P47T)",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     breaks = 100)
+
+# Transformación logarítmica para mejorar la visualizacion
+argentinos_entre_18y65 %>%
+  filter(P47T > 0) %>%  # evitar log(0)
+  ggplot(aes(x = log(P47T))) +
+  geom_histogram(binwidth = 0.2, fill = "steelblue", color = "white") +
+  labs(title = "Histograma log(P47T)",
+       x = "Logaritmo del ingreso",
+       y = "Frecuencia") +
+  theme_minimal()
+
+# qq-plot
+qqnorm((argentinos_entre_18y65 %>% filter(P47T > 0))$P47T)
+qqline((argentinos_entre_18y65 %>% filter(P47T > 0))$P47T, col = "red")
+# Como los ingresos suelen estar sesgados, lo mejor es hacer el Q-Q plot sobre el logaritmo:
+qqnorm(log((argentinos_entre_18y65 %>% filter(P47T > 0))$P47T))
+qqline(log((argentinos_entre_18y65 %>% filter(P47T > 0))$P47T), col = "blue")
+
+
+hist(ingresos_extranjeros,
+     main = "Histograma de P47T",
+     xlab = "Ingreso (P47T)",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     breaks = 130)
+
+# Transformación logarítmica para mejorar la visualizacion
+extranjeros_entre_18y65 %>%
+  filter(P47T > 0) %>%  # evitar log(0)
+  ggplot(aes(x = log(P47T))) +
+  geom_histogram(binwidth = 0.2, fill = "steelblue", color = "white") +
+  labs(title = "Histograma log(P47T)",
+       x = "Logaritmo del ingreso",
+       y = "Frecuencia") +
+  theme_minimal()
+
+
+# Aplicar el test de Anderson-Darling
+ad.test(ingresos_argentinos)
+ad.test(ingresos_extranjeros)
+
+# ambas muestras no pasan el test de normalidad, solo nos queda asumir que n es lo suficientemente grande
+MeanCI(x = ingresos_argentinos, conf.level = 0.95)
+MeanCI(x = ingresos_extranjeros, conf.level = 0.95)
+
+Los intervalos se superponen con lo cual no podemos concluir que alguno de los promedios es mayor que el otro con una confianza del 95%
+
 
 # GRÁFICO N° 4: Comparación de edad entre argentinos y extranjeros residentes.
 # Calcular estadísticas por grupo para usar en las etiquetas
